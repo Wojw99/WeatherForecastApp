e9a2d3a9d3ee888045130441a6a220d311bf0044
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-06-19 15:48:36 +0200

    First commit
    First commit

---
+++ WeatherForecastApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherForecastApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonToday_Click(object sender, RoutedEventArgs e)
+        {
+            panelToday.Visibility = Visibility.Visible;
+            panelWeek.Visibility = Visibility.Hidden;
+            buttonToday.IsEnabled = false;
+            buttonWeek.IsEnabled = true;
+        }
+
+        private void ButtonWeek_Click(object sender, RoutedEventArgs e)
+        {
+            panelToday.Visibility = Visibility.Hidden;
+            panelWeek.Visibility = Visibility.Visible;
+            buttonToday.IsEnabled = true;
+            buttonWeek.IsEnabled = false;
+        }
+    }
+}

aadeb0d73494f27922cacf2bc3d231c8cceeeb4b
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-06-28 10:58:57 +0200

    Weather+

--- WeatherForecastApp/Class/Weather.cs
+++ WeatherForecastApp/Class/Weather.cs
@@ -8,7 +8,13 @@ namespace WeatherForecastApp.Class
 {
     public class Weather
     {
+        public string City { get; set; }
+
         public double Temp { get; set; }
+        public double Pressure { get; set; }
+        public double Clouds { get; set; }
+        public double WindSpeed { get; set; }
+
         public DateTime Date { get; set; }
 
         public Weather(double temp, DateTime date)
---
+++ WeatherForecastApp/Class/WeatherApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastApp.Class
+{
+    public class WeatherApi
+    {
+        private string url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=current,minutely,hourly&appid=54443f860b9257ccbab2a8815b5f3e0f";
+    }
+}

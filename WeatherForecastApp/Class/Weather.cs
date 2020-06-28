408f873d61a91c202c170a62692d25dd32e32c3a
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-06-28 10:40:45 +0200

    Weather

---
+++ WeatherForecastApp/Class/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecastApp.Class
+{
+    public class Weather
+    {
+        public double Temp { get; set; }
+        public DateTime Date { get; set; }
+
+        public Weather(double temp, DateTime date)
+        {
+            Temp = temp;
+            Date = date;
+        }
+    }
+}

d3426e8f276b607938c03bc5bac7e6bd39d84f95
Paweł Piłat <pawelpilat999@interia.pl>
2020-07-01 20:47:34 +0200

    tak

--- WeatherForecastApp/Class/Weather.cs
+++ WeatherForecastApp/Class/Weather.cs
@@ -10,16 +10,20 @@ namespace WeatherForecastApp.Class
     {
         public string City { get; set; }
 
-        public double Temp { get; set; }
+        public double Temperature { get; set; }
         public double Pressure { get; set; }
         public double Clouds { get; set; }
         public double WindSpeed { get; set; }
 
         public DateTime Date { get; set; }
 
-        public Weather(double temp, DateTime date)
+        public Weather(string city, double temperature, double pressure, double clouds, double windSpeed, DateTime date)
         {
-            Temp = temp;
+            City = city;
+            Temperature = temperature;
+            Pressure = pressure;
+            Clouds = clouds;
+            WindSpeed = windSpeed;
             Date = date;
         }
     }

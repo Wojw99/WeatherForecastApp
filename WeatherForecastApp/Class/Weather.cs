aa291f1a0b9ec3583e266842e60ade7e37673f2c
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-07-02 19:38:51 +0200

    poprawka

--- WeatherForecastApp/Class/Weather.cs
+++ WeatherForecastApp/Class/Weather.cs
@@ -8,23 +8,9 @@ namespace WeatherForecastApp.Class
 {
     public class Weather
     {
-        public string City { get; set; }
-
-        public double Temperature { get; set; }
-        public double Pressure { get; set; }
-        public double Clouds { get; set; }
-        public double WindSpeed { get; set; }
-
-        public DateTime Date { get; set; }
-
-        public Weather(string city, double temperature, double pressure, double clouds, double windSpeed, DateTime date)
-        {
-            City = city;
-            Temperature = temperature;
-            Pressure = pressure;
-            Clouds = clouds;
-            WindSpeed = windSpeed;
-            Date = date;
-        }
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
     }
 }
